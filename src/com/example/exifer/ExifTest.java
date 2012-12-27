0cf57a9d530dce91d44a8cc2bbebd3d2ba81d167
ENDO Yasuyuki <eyasuyuki@gmail.com>
2012-12-27 09:31:25 +0900

    add

---
+++ src/com/example/exifer/ExifTest.java
@@ -0,0 +1,63 @@
+package com.example.exifer;
+
+import java.io.File;
+import java.util.Date;
+import javax.activation.MimetypesFileTypeMap;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
+
+public class ExifTest {
+	
+	void retrieve(File dir) {
+		File[] files = dir.listFiles();
+		if (files == null || files.length <= 0) return;
+		for (File f: files) {
+			if (f.isFile())      processFile(f);
+			if (f.isDirectory()) retrieve(f);
+		}
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length <= 0) {
+			usage();
+			return;
+		}
+		File file = new File(args[0]);
+		ExifTest test = new ExifTest();
+//		test.processFile(file);
+		test.retrieve(file);
+	}
+	
+	void processFile(File file) {
+		MimetypesFileTypeMap m = new MimetypesFileTypeMap();
+		if (m == null) return;
+		String contentType = m.getContentType(file);
+		if (contentType == null) return;
+		if (!contentType.equals("image/jpeg")) return;
+		try {
+			Metadata metadata = ImageMetadataReader.readMetadata(file);
+			if (metadata == null) return;
+			Directory dir = metadata.getDirectory(ExifIFD0Directory.class);
+			if (dir == null) return;
+			String path = file.getAbsolutePath();
+			String name = file.getName();
+			long size = file.length();
+			Date date = dir.getDate(ExifIFD0Directory.TAG_DATETIME);
+			System.out.println("path="+path+", name="+name+", size="+size+", date="+date);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void usage() {
+		System.out.println("filename missing.");
+	}
+
+}
