52130dfe58ed11d09af306c960cb23af71ee1a73
ENDO Yasuyuki <eyasuyuki@gmail.com>
2012-12-27 09:39:14 +0900

    add throws to retrieve, processFile

--- src/com/example/exifer/ExifTest.java
+++ src/com/example/exifer/ExifTest.java
@@ -1,17 +1,19 @@
 package com.example.exifer;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import javax.activation.MimetypesFileTypeMap;
 
 import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifIFD0Directory;
 
 public class ExifTest {
 	
-	void retrieve(File dir) {
+	void retrieve(File dir) throws ImageProcessingException, IOException {
 		File[] files = dir.listFiles();
 		if (files == null || files.length <= 0) return;
 		for (File f: files) {
@@ -32,28 +34,31 @@ public class ExifTest {
 		File file = new File(args[0]);
 		ExifTest test = new ExifTest();
 //		test.processFile(file);
-		test.retrieve(file);
-	}
-	
-	void processFile(File file) {
-		MimetypesFileTypeMap m = new MimetypesFileTypeMap();
-		if (m == null) return;
-		String contentType = m.getContentType(file);
-		if (contentType == null) return;
-		if (!contentType.equals("image/jpeg")) return;
 		try {
-			Metadata metadata = ImageMetadataReader.readMetadata(file);
-			if (metadata == null) return;
-			Directory dir = metadata.getDirectory(ExifIFD0Directory.class);
-			if (dir == null) return;
-			String path = file.getAbsolutePath();
-			String name = file.getName();
-			long size = file.length();
-			Date date = dir.getDate(ExifIFD0Directory.TAG_DATETIME);
-			System.out.println("path="+path+", name="+name+", size="+size+", date="+date);
+			test.retrieve(file);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	void processFile(File file) throws ImageProcessingException, IOException {
+		MimetypesFileTypeMap m = new MimetypesFileTypeMap();
+		if (m == null) return;
+		
+		String contentType = m.getContentType(file);
+		if (contentType == null || !contentType.equals("image/jpeg")) return;
+		
+		Metadata metadata = ImageMetadataReader.readMetadata(file);
+		if (metadata == null) return;
+		
+		Directory dir = metadata.getDirectory(ExifIFD0Directory.class);
+		if (dir == null) return;
+		
+		String path = file.getAbsolutePath();
+		String name = file.getName();
+		long size = file.length();
+		Date date = dir.getDate(ExifIFD0Directory.TAG_DATETIME);
+		System.out.println("path="+path+", name="+name+", size="+size+", date="+date);
 	}
 	
 	static void usage() {
